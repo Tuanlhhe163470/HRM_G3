ba88e0710279aa6e539d8f271275201d7beca4b5
Chien Khong <92623702+kvanchien@users.noreply.github.com>
2026-02-04 13:57:24 +0700

    update contact and introduction page

--- HRM/HRM_Frontend/src/app/(public)/gioi-thieu-tong-quan/page.jsx
+++ HRM/HRM_Frontend/src/app/(public)/gioi-thieu-tong-quan/page.jsx
@@ -1,18 +1,9 @@
 "use client"
-import BusinessPhilosophy from "./component/BusinessPhilosophy"
-import Statistics from "./component/Statistics"
-import TitleSection from "./component/TitleSection"
+
 export default function Page() {
   return (
     <div className=" max-w-7xl mx-auto" style={{ marginTop: "50px" }}>
-      {/* Tiêu đề & mô tả */}
-      <TitleSection />
 
-      {/* Phần thống kê */}
-      <Statistics />
-
-      {/* Triết lý kinh doanh */}
-      <BusinessPhilosophy />
     </div>
   )
 }
